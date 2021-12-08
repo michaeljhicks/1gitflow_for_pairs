137cd90406f24aca6a94cf35e3f8439f4e47dece
Michael Hicks <hicks.michael04@gmail.com>
2021-12-08 12:16:40 -0700

    change to first line

--- 1filename.rb
+++ 1filename.rb
@@ -1,4 +1,5 @@
-nothing added to commit but untracked files present (use "git add" to track)
+first line change
+dded to commit but untracked files present (use "git add" to track)
 /Users/michaelhicks/turing/1module/practice/1gitflow_for_pairs main $git init
 Initialized empty Git repository in /Users/michaelhicks/turing/1module/practice/1gitflow_for_pairs/.git/
 /Users/michaelhicks/turing/1module/practice/1gitflow_for_pairs main $git remote add origin git@github.com:michaeljhicks/1gitflow_for_pairs.git
