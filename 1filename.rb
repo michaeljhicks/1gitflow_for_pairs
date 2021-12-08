8d6fa631f214b5db0355664fa8f775f9ef730a17
Michael Hicks <hicks.michael04@gmail.com>
2021-12-08 12:12:25 -0700

    Initial commit

---
+++ 1filename.rb
@@ -0,0 +1,8 @@
+nothing added to commit but untracked files present (use "git add" to track)
+/Users/michaelhicks/turing/1module/practice/1gitflow_for_pairs main $git init
+Initialized empty Git repository in /Users/michaelhicks/turing/1module/practice/1gitflow_for_pairs/.git/
+/Users/michaelhicks/turing/1module/practice/1gitflow_for_pairs main $git remote add origin git@github.com:michaeljhicks/1gitflow_for_pairs.git
+/Users/michaelhicks/turing/1module/practice/1gitflow_for_pairs main $git remote -v
+origin	git@github.com:michaeljhicks/1gitflow_for_pairs.git (fetch)
+origin	git@github.com:michaeljhicks/1gitflow_for_pairs.git (push)
+/Users/michaelhicks/turing/1module/practice/1gitflow_for_pairs main $git status
